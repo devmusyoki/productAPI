95b60b919e2f80eff053031f287d568688ac4652
smusyoki <markmusyoki6@gmail.com>
2022-03-13 15:14:59 +0300

    update

--- projecta/models.py
+++ projecta/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    Category = models.ForeignKey(Category, on_delete=models.PROTECT, default =1 )
+    name = models.CharField(max_length=100)
+    price = models.FloatField(max_length=100)
+    image = models.ImageField()
+    slug = models.SlugField(max_length=250)
+    published = models.DateTimeField(default = timezone.now())
+
+    def __str__(self):
+        return self.name
