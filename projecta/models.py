274ffdde268c9daebcbf284dd4e0a0acf4e13c77
smusyoki <markmusyoki6@gmail.com>
2022-03-14 10:15:47 +0300

    update

--- projecta/models.py
+++ projecta/models.py
@@ -8,12 +8,28 @@ class Category(models.Model):
         return self.name
     
 class Product(models.Model):
+    
+    class PostObjects(models.Manager):
+        def get_queryset(self):
+            return super(Product, self).get_queryset() .filter(status='published')
+    
+    options = (
+           ('draft', 'Draft'),
+           ('published', 'Published')
+       )
+    
     Category = models.ForeignKey(Category, on_delete=models.PROTECT, default =1 )
     name = models.CharField(max_length=100)
     price = models.FloatField(max_length=100)
     image = models.ImageField()
     slug = models.SlugField(max_length=250)
     published = models.DateTimeField(default = timezone.now())
+    status = models.CharField(max_length=10, choices=options, default = 'published',)
+    objects = models.Manager() # default manager
+    
+    class Meta:
+        ordering = ('-published')
+ 
 
     def __str__(self):
         return self.name
--- projecta_api/views.py
+++ projecta_api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from projecta.models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+class ProductList(generic.ListCreateAPIView):
+    queryset = Product.productobjects.all()
+    serializer_class = ProductSerializer
+    pass
+
+class ProductDetail(generics.RetrieveDestroyAPIView):
+    pass
