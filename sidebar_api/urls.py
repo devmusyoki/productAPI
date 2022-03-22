2e880a6999e06c110184b658f7fddcd0791beb3f
smusyoki <markmusyoki6@gmail.com>
2022-03-22 15:52:06 +0300

    update

--- sidebar_api/urls.py
+++ sidebar_api/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
-from .views import SidebarList, SidebarDetail
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import SidebarList, SidebarDetail, SidebarViewSet
 
+
+router = DefaultRouter()
+router.register('sidebar', SidebarViewSet)
 app_name = "sidebar_api"
 
 urlpatterns = [
     path('<int:pk>/', SidebarDetail.as_view(), name='detailcreate'),
     path('', SidebarList.as_view(), name='listcreate'),
+    path('api/',include(router.urls))
 ]
--- sidebar_api/views.py
+++ sidebar_api/views.py
@@ -1,4 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.viewsets import ModelViewSet
 from projecta.models import Category
 from .serializers import CategorySerializer
 from rest_framework import generics
@@ -15,3 +16,7 @@ class SidebarDetail(generics.RetrieveDestroyAPIView):
     queryset = Category.objects.filter(parent=None)
     serializer_class = CategorySerializer
     pass
+
+class SidebarViewSet (ModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.filter(parent=None)
